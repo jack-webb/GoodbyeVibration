cc92f20e1313b348cfe987a117ebde68879bf7d2
Jack Webb <git@jackwebb.me>
2021-05-21 08:55:20 +0100

    Set up nav with action bar properly (Fixes #9)

--- GoodbyeVibration/app/src/main/java/me/jackwebb/goodbyevibration/GoodbyeVibrationActivity.kt
+++ GoodbyeVibration/app/src/main/java/me/jackwebb/goodbyevibration/GoodbyeVibrationActivity.kt
@@ -3,6 +3,7 @@ package me.jackwebb.goodbyevibration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,6 +13,7 @@ class GoodbyeVibrationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
     }
 
     override fun onSupportNavigateUp(): Boolean {
